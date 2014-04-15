92c6af6622863f08e5a6b0a3f204e0d12b21a354
Eduard Luhtonen <luhtonen@gmail.com>
2014-04-15 08:44:38 +0200

    adding user authentication and authorisation
    Adding user authentication and authorisation with support of devise and
    cancan gems

--- rails-projects/todo-new/app/controllers/tasks_controller.rb
+++ rails-projects/todo-new/app/controllers/tasks_controller.rb
@@ -3,32 +3,42 @@ class TasksController < ApplicationController
     @task = Task.new
     render :show_form
   end
+
   def create
     @task = Task.new(task_params)
+    @task.user = current_user
+    authorize! :create, @task
     save_task
   end
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    @tasks = Task.all
-  end
+
   def edit
     @task = Task.find(params[:id])
+    authorize! :edit, @task
     render :show_form
   end
+
   def update
     @task = Task.find(params[:id])
     @task.assign_attributes(task_params)
+    authorize! :update, @task
     save_task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    authorize! :destroy, @task
+    @task.destroy
+    @tasks = Task.all
   end
 
   private
   def task_params
     params.require(:task).permit(:title, :note, :completed)
   end
+
   def save_task
     if @task.save
-      @tasks = Task.all
+      @tasks = Task.accessible_by(current_ability)
       render :hide_form
     else
       render :show_form
--- rails-projects/todo-new/config/routes.rb
+++ rails-projects/todo-new/config/routes.rb
@@ -1,4 +1,5 @@
 TodoNew::Application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   resources :tasks, except: [:index]
 end
