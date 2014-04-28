2c237d298038999029a6f41a2dde1c6338acfa2e
Eduard Luhtonen <luhtonen@gmail.com>
2014-04-28 21:25:17 +0200

    hello with coloured console

--- NodeJS_Succinctly/helloworld.js
+++ NodeJS_Succinctly/helloworld.js
@@ -1,1 +1,5 @@
+var clc = require('cli-color');
 console.log('Hello World, nodejs!');
+console.log(clc.blue('"Hello node.js" in blue'));
+console.log(clc.red('"Hello node.js" in red'));
+console.log(clc.green('"Hello node.js" in green'));
