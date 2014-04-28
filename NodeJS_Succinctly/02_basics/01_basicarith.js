d06c1adae64854d1bd0a386e08e6a38ca17e9a73
Eduard Luhtonen <luhtonen@gmail.com>
2014-04-28 22:03:06 +0200

    NodeJS basics

---
+++ NodeJS_Succinctly/02_basics/01_basicarith.js
@@ -0,0 +1,19 @@
+var a, b;
+a = 10;
+b = 5.4;
+
+// Addition
+var c = a + b;
+console.log(c);
+
+// Substitution
+c = a - b;
+console.log(c);
+
+// Multiplication
+c = a * b;
+console.log(c);
+
+// Division
+c = a / b;
+console.log(c);
