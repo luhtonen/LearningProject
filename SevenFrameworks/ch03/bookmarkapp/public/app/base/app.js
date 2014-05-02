2bd16b3edf0c01c6eb25a965eb7734bafc796df5
Eduard Luhtonen <luhtonen@gmail.com>
2014-05-02 08:46:32 +0200

    application initial version with tests

--- SevenFrameworks/ch03/bookmarkapp/public/app/base/app.js
+++ SevenFrameworks/ch03/bookmarkapp/public/app/base/app.js
@@ -1,1 +1,22 @@
-angular.module("App_base", ["ngResource"]);
+var app = angular.module("App_base", ["ngResource"]);
+app.factory("Bookmark", function($resource) {
+    return $resource("/bookmarks/:id", {id:"@id"});
+});
+app.factory("bookmarks", function(Bookmark) {
+    return Bookmark.query();
+});
+app.factory("saveBookmark", function(bookmarks) {
+    return function(bookmark) {
+        if (!bookmark.id) {
+            bookmarks.push(bookmark);
+        }
+        bookmark.$save();
+    };
+});
+app.factory("deleteBookmark", function(bookmarks) {
+    return function(bookmark) {
+        var index = bookmarks.indexOf(bookmark);
+        bookmark.$delete();
+        bookmarks.splice(index, 1);
+    };
+});
