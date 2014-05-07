ac2a6cab69e10ec9eb7e858cc14a15ecc29685de
Eduard Luhtonen <luhtonen@gmail.com>
2014-05-07 22:11:31 +0200

    filtering by tags

--- SevenFrameworks/ch03/bookmarkapp/public/app/base/app.js
+++ SevenFrameworks/ch03/bookmarkapp/public/app/base/app.js
@@ -26,17 +26,76 @@ app.service("state", function(Bookmark) {
     this.clearForm = function() {
         this.formBookmark.bookmark = new Bookmark();
     };
+    this.bookmarkFilter = {filterTag:""};
 });
 app.factory("editBookmark", function(state) {
     return function(bookmark) {
         state.formBookmark.bookmark = bookmark;
     }
 });
+app.factory("buildTagList", function() {
+    return function(bookmarks) {
+        var bookmarkCounts = {};
+
+        bookmarks.forEach(function(bookmark) {
+            var tagList = bookmark.tagList;
+
+            tagList.forEach(function(tag) {
+                var existing = bookmarkCounts[tag];
+                bookmarkCounts[tag] = existing ? existing + 1 : 1;
+            });
+        });
+
+        var labels = Object.keys(bookmarkCounts);
+        labels.sort();
+        return labels.map(function(label) {
+            return {label: label, bookmarkCount: bookmarkCounts[label]};
+        });
+    };
+});
+app.filter("filterByTag", function() {
+    var byTag = function(filterTag) {
+        return function(bookmark) {
+            var tagList = bookmark.tagList;
+            var noFilter = (!filterTag) || (filterTag.length == 0);
+            var tagListContainsFilterTag = tagList && tagList.indexOf(filterTag) > -1;
+            return noFilter || tagListContainsFilterTag;
+        };
+    };
+    return function(bookmarks, filterTag) {
+        return bookmarks.filter(byTag(filterTag));
+    };
+});
 app.controller("BookmarkListController",
-    function($scope, bookmarks, deleteBookmark, editBookmark) {
+    function($scope, state, bookmarks, deleteBookmark, editBookmark) {
         $scope.bookmarks = bookmarks;
+        $scope.bookmarkFilter = state.bookmarkFilter;
+        $scope.filterBy = function(tag) {
+            state.bookmarkFilter.filterTag = tag;
+        };
         $scope.deleteBookmark = deleteBookmark;
         $scope.editBookmark = editBookmark;
+    }
+);
+app.controller("TagListController",
+    function($scope, state, bookmarks, buildTagList) {
+        $scope.bookmarks = bookmarks;
+        $scope.$watch("bookmarks", function(updatedBookmarks) {
+            $scope.tags = buildTagList(updatedBookmarks);
+        }, true); // true compares objects for equality rather than by reference
+
+        $scope.filterBy = function(tag) {
+            state.bookmarkFilter.filterTag = tag.label;
+        };
+    }
+);
+app.controller("TagFilterController",
+    function($scope, state) {
+        $scope.bookmarkFilter = state.bookmarkFilter;
+
+        $scope.clearFilter = function() {
+            state.bookmarkFilter.filterTag = "";
+        };
     }
 );
 app.controller("BookmarkFormController",
