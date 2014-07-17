383bb7b0f046ee53d7df4db2f39639dcb69f710e
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-17 12:29:36 +0200

    replacing element using directive

--- AngularJS_Succinctly/ch03/js/ch03_03.js
+++ AngularJS_Succinctly/ch03/js/ch03_03.js
@@ -3,6 +3,7 @@ var app = angular.module('MyApp', []);
 app.directive('myWidget', function() {
     return {
         restrict: 'E',
+        replace: true,
         template: "<p>Hello World</p>"
     };
 });
