1e8e43965e4474c9c81786c013b21b3657132a9f
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-17 18:22:50 +0200

    transclude and directive as attribute

---
+++ AngularJS_Succinctly/ch03/js/ch03_05.js
@@ -0,0 +1,12 @@
+var app = angular.module('MyApp', []);
+
+app.directive('myWidget', function() {
+    var linkFunction = function(scope, element, attributes) {
+        scope.text = attributes["myWidget"];
+    };
+    return {
+        restrict: "A",
+        template: "<p>{{text}}</p>",
+        link: linkFunction
+    };
+});
