0731cb0cbb311c99badde2eac6457a6b63b987ab
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-18 09:37:16 +0200

    experiments with transclude

--- AngularJS_Succinctly/ch03/js/ch03_04.js
+++ AngularJS_Succinctly/ch03/js/ch03_04.js
@@ -4,6 +4,6 @@ app.directive('myWidget', function() {
     return {
         restrict: "E",
         transclude: true,
-        template: "<div ng-transclude><h3>Heading</h3></div>"
+        templateUrl: "widget04.html"
     };
 });
