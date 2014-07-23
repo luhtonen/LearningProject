7ecc5088e3fcf5f2edb8a7eb676388245ea2a0b8
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-23 08:59:31 +0200

    filtering arrays

---
+++ AngularJS_Succinctly/ch04/js/ch04_04.js
@@ -0,0 +1,13 @@
+var app = angular.module('MyApp', []);
+
+app.filter('exclude', function() {
+    return function(input, exclude) {
+        var result = [];
+        for (var i = 0; i < input.length; i++) {
+            if (input[i] !== exclude) {
+                result.push(input[i]);
+            }
+        }
+        return result;
+    };
+});
