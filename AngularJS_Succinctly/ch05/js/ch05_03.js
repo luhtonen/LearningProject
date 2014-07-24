2841f821a71a96cda870247e820ff7e8dafbd83d
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-24 11:46:55 +0200

    switching to use new twitter api
    New Twitter API requires authentication, so this example is not working
    as it is, but generally calling external services from client (in this
    case from JavaScript code) can be problematic because of CORS problem

--- AngularJS_Succinctly/ch05/js/ch05_03.js
+++ AngularJS_Succinctly/ch05/js/ch05_03.js
@@ -2,8 +2,7 @@ var app = angular.module('MyApp', ['ngResource']);
 
 app.controller('MyCtrl', ['$scope', '$resource',
     function($scope, $resource) {
-        var TwitterAPI = $resource("http://search.twitter.com/search.json",
-            { callback: "JSON_CALLBACK" },
+        var TwitterAPI = $resource("https://api.twitter.com/1.1/search/",
             { get: { method: "JSONP" }});
         $scope.search = function() {
             $scope.searchResult = TwitterAPI.get({ q: $scope.searchTerm });
