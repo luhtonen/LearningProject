43c087c6b7e9a0bbf5cce145149b79bd276d41cd
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-29 17:00:03 +0200

    displaying flash messages

---
+++ AngularJS_Succinctly/ch08/js/ch08_05.js
@@ -0,0 +1,32 @@
+var app = angular.module('MyApp', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider.
+        when("/home", { templateUrl: "home.html"}).
+        when("/page", { templateUrl: "page.html"}).
+        otherwise({redirectTo: "/home"});
+});
+
+app.factory('flash', function($rootScope) {
+    var queue = [];
+    var currentMessage = '';
+
+    $rootScope.$on('$routeChangeSuccess', function() {
+        currentMessage = queue.shift() || '';
+    });
+    return {
+        setMessage: function(message) {
+            queue.push(message);
+        },
+        getMessage: function() {
+            return currentMessage;
+        }
+    }
+});
+app.controller('MyCtrl', ['$scope', '$location', 'flash', function($scope, $location, flash) {
+    $scope.flash = flash;
+    $scope.message = 'Hello World';
+    $scope.submit = function(message) {
+        flash.setMessage(message);
+        $location.path("/page");
+    };
+}]);
