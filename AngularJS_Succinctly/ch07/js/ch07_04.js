fe7c23291c0ef8c24bc50239c765b7744da9a116
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-29 09:48:04 +0200

    form validation with twitter bootstrap styles

---
+++ AngularJS_Succinctly/ch07/js/ch07_04.js
@@ -0,0 +1,14 @@
+var app = angular.module('MyApp', []);
+
+app.controller('User', ['$scope', function($scope) {
+    $scope.user = {};
+    $scope.wasSubmitted = false;
+
+    $scope.submit = function() {
+        $scope.wasSubmitted = true;
+    };
+    $scope.error = function(name) {
+        var s = $scope.form[name];
+        return s.$invalid && s.$dirty ? 'has-error' : '';
+    }
+}]);
