65cea9e8a9ae330670857545eda6f5ff0c5ad9d2
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-29 12:50:44 +0200

    more complex filter expression

--- AngularJS_Succinctly/ch08/js/ch08_01.js
+++ AngularJS_Succinctly/ch08/js/ch08_01.js
@@ -4,8 +4,8 @@ app.controller('MyCtrl', ['$scope', function($scope) {
         { name: 'Anton', age: 46 },
         { name: 'Nastya', age: 47 },
         { name: 'Anastassia', age: 27 },
-        { name: 'Vitya', age: 55 },
-        { name: 'Maija', age: 48 },
+        { name: 'Vitya', age: 57 },
+        { name: 'Maija', age: 47 },
         { name: 'Robert', age: 43 }
     ];
 }]);
