2404aca6ce15833464312df34dca62195554f6c5
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-30 15:12:13 +0200

    angular template

--- my-angular-phonecat/app/scripts/controllers/main.js
+++ my-angular-phonecat/app/scripts/controllers/main.js
@@ -8,10 +8,14 @@
  * Controller of the myAngularPhonecatApp
  */
 angular.module('myAngularPhonecatApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+.controller('PhoneListCtrl', function ($scope) {
+    $scope.phones = [
+        {'name': 'Nexus S',
+         'snippet': 'Fast just got faster with Nexus S.'},
+        {'name': 'Motorola XOOM™ with Wi-Fi',
+         'snippet': 'The Next, Next Generation tablet.'},
+        {'name': 'MOTOROLA XOOM™',
+         'snippet': 'The Next, Next Generation tablet.'}
     ];
-  });
+    $scope.name = 'World';
+});
--- my-angular-phonecat/test/spec/controllers/main.js
+++ my-angular-phonecat/test/spec/controllers/main.js
@@ -5,18 +5,22 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('myAngularPhonecatApp'));
 
-  var MainCtrl,
+  var PhoneListCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+      PhoneListCtrl = $controller('PhoneListCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should create "phones" model with 3 phones', function () {
+    expect(scope.phones.length).toBe(3);
+  });
+
+  it('should have name equal to "World"', function() {
+      expect(scope.name).toBe('World');
   });
 });
