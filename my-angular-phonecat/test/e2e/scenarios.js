fa70ac3db3886f118bb7beef46655289108553cd
Eduard Luhtonen <luhtonen@gmail.com>
2014-07-31 09:32:15 +0200

    experiments with e2e tests

--- my-angular-phonecat/test/e2e/scenarios.js
+++ my-angular-phonecat/test/e2e/scenarios.js
@@ -4,9 +4,10 @@ describe('PhoneCat App', function() {
             browser.get('/');
         });
 
+        var phoneList = element.all(by.repeater('phone in phones'));
+        var query = element(by.model('query'));
+
         it('should filter the phone list as user types into the search box', function() {
-            var phoneList = element.all(by.repeater('phone in phones'));
-            var query = element(by.model('query'));
 
             expect(phoneList.count()).toBe(3);
 
@@ -16,6 +17,11 @@ describe('PhoneCat App', function() {
             query.clear();
             query.sendKeys('motorola');
             expect(phoneList.count()).toBe(2);
+        });
+
+        it('should display the current filter value in the title bar', function() {
+            query.clear();
+            expect(browser.getTitle()).toMatch(/Google Phone Gallery$/);
         });
     });
 });
