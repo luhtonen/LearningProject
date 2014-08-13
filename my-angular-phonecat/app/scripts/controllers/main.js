a15ed4b445621e81570d61939043fcb9f4a60187
Eduard Luhtonen <luhtonen@gmail.com>
2014-08-13 07:09:35 +0200

    clickable thumbnails

--- my-angular-phonecat/app/scripts/controllers/main.js
+++ my-angular-phonecat/app/scripts/controllers/main.js
@@ -17,5 +17,10 @@ angular.module('myAngularPhonecatApp')
     .controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
         $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
             $scope.phone = data;
+            $scope.mainImageUrl = data.images[0];
         });
+
+        $scope.setImage = function(imageUrl) {
+            $scope.mainImageUrl = imageUrl;
+        };
     }]);
