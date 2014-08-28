26b50106c4d6c2722e6eb3401da40fcec7e21f79
Eduard Luhtonen <luhtonen@gmail.com>
2014-08-28 07:56:43 +0200

    animating ngClass with JavaScript

--- my-angular-phonecat/app/scripts/animations.js
+++ my-angular-phonecat/app/scripts/animations.js
@@ -1,1 +1,48 @@
-angular.module('phonecatAnimations', ['ngAnimate']);
+'use strict';
+
+var phonecatAnimations = angular.module('phonecatAnimations', ['ngAnimate']);
+phonecatAnimations.animation('.phone', function() {
+  var animateUp = function(element, className, done) {
+    if (className !== 'active') {
+      return;
+    }
+    element.css({
+      position: 'absolute',
+      top: 500,
+      left: 0,
+      display: 'block'
+    });
+    jQuery(element).animate({
+      top: 0
+    }, done);
+    return function(cancel) {
+      if (cancel) {
+        element.stop();
+      }
+    };
+  };
+
+  var animateDown = function(element, className, done) {
+    if (className !== 'active') {
+      return;
+    }
+    element.css({
+      position: 'absolute',
+      top: 0,
+      left: 0
+    });
+    jQuery(element).animate({
+      top: -500
+    }, done);
+    return function(cancel) {
+      if (cancel) {
+        element.stop();
+      }
+    };
+  };
+
+  return {
+    addClass: animateUp,
+    removeClass: animateDown
+  };
+});
