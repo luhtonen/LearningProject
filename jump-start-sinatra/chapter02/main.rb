a06ba9de1bf326fdb6cfa75906e77598ba051439
Eduard Luhtonen <luhtonen@gmail.com>
2014-09-23 16:32:37 +0200

    initial version of Sinatra site

---
+++ jump-start-sinatra/chapter02/main.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  erb :home
+end
+
+__END__
+@@home
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Songs by Sinatra</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <header>
+      <h1>Songs by Sinatra</h1>
+      <nav>
+        <ul>
+          <li><a href="/" title="Home">Home</a></li>
+          <li><a href="/about" title="About">About</a></li>
+          <li><a href="/contact" title="Contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section>
+      <p>Welcome to this website all about the songs of the great Frank Sinatra</p>
+    </section>
+  </body>
+</html>
