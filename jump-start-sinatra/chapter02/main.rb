d854849939373d7075b5ffb069068a2b6256b707
Eduard Luhtonen <luhtonen@gmail.com>
2014-09-24 09:48:03 +0200

    replacing ERB with Slim

--- jump-start-sinatra/chapter02/main.rb
+++ jump-start-sinatra/chapter02/main.rb
@@ -1,19 +1,20 @@
 require 'sinatra'
+require 'slim'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  erb :home
+  slim :home
 end
 
 get '/about' do
   @title = 'All About This Website'
-  erb :about
+  slim :about
 end
 
 get '/contact' do
-  erb :contact
+  slim :contact
 end
 
 not_found do
-  erb :not_found
+  slim :not_found
 end
