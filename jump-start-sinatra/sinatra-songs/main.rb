317682a89fdeb7a4db1170170b203060a007b609
Eduard Luhtonen <luhtonen@gmail.com>
2014-09-26 09:52:14 +0200

    adding login logic

--- jump-start-sinatra/sinatra-songs/main.rb
+++ jump-start-sinatra/sinatra-songs/main.rb
@@ -5,6 +5,8 @@ require './song'
 
 configure do
   enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
 end
 
 get('/styles.css') { scss :styles }
@@ -24,6 +26,19 @@ end
 
 not_found do
   slim :not_found
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/songs')
+  else
+    slim :login
+  end
 end
 
 get '/set/:name' do
