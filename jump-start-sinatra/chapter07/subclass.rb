8293deb4c0e70b1c05066cdba7fc49ab948dccde
Eduard Luhtonen <luhtonen@gmail.com>
2014-10-02 08:56:06 +0200

    experiments with subclasses

---
+++ jump-start-sinatra/chapter07/subclass.rb
@@ -0,0 +1,23 @@
+require 'sinatra/base'
+
+class App < Sinatra::Base
+  set :name, 'App'
+
+  get '/' do
+    'this is the app'
+  end
+
+  get '/hello' do
+    "Hello, this is #{settings.name}"
+  end
+end
+
+class Sub < App
+  set :name, 'Sub'
+
+  get '/' do
+    'This is the sub app'
+  end
+end
+
+Sub.run!
