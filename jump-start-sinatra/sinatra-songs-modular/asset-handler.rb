673e3cd5b7e041289ec1a7b7d0355c3830b9f382
Eduard Luhtonen <luhtonen@gmail.com>
2014-10-02 09:46:39 +0200

    caching assets

--- jump-start-sinatra/sinatra-songs-modular/asset-handler.rb
+++ jump-start-sinatra/sinatra-songs-modular/asset-handler.rb
@@ -9,10 +9,14 @@ class AssetHandler < Sinatra::Base
 
   get '/javascript/*.js' do
     pass unless settings.coffeescript?
+    last_modified File.mtime(settings.root + '/assets/' + settings.jsdir)
+    cache_control :public, :must_revalidate
     coffee (settings.jsdir + '/' + params[:splat].first).to_sym
   end
 
   get '/*.css' do
+    last_modified File.mtime(settings.root + '/assets/' + settings.cssdir)
+    cache_control :public, :must_revalidate
     send(settings.cssengine, (settings.cssdir + '/' + params[:splat].first).to_sym)
   end
 end
