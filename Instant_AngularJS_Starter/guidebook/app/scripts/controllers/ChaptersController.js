6f0b792f9254f423c04e7d77ae6b93d14adc4d96
luhtonen <luhtonen@gmail.com>
2014-10-09 13:19:40 +0200

    defining controllers

--- Instant_AngularJS_Starter/guidebook/app/scripts/controllers/ChaptersController.js
+++ Instant_AngularJS_Starter/guidebook/app/scripts/controllers/ChaptersController.js
@@ -8,10 +8,18 @@
  * Controller of the GuidebookApp.Guidebook
  */
 angular.module('GuidebookApp.Guidebook')
-  .controller('ChaptersController', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('ChaptersController', ['$scope', '$location', '$routeParams', 'ChapterModel', 'NoteModel',
+    function ($scope, $location, $routeParams, ChapterModel, NoteModel) {
+      var chapters = ChapterModel.getChapters();
+      for (var i = 0; i < chapters.length; i++) {
+        chapters[i].notes = NoteModel.getNotesForChapter(chapters[i].id);
+      }
+      $scope.chapters = chapters;
+      $scope.selectedChapterId = $routeParams.chapterId;
+      $scope.onDelete = function(noteId) {
+        var confirmDelete = confirm('Are your sure you want to delete this note?');
+        if (confirmDelete) {
+          $location.path('/guidebook/deleteNote/' + $routeParams.chapterId + '/' + noteId);
+        }
+      };
+  }]);
--- Instant_AngularJS_Starter/guidebook/app/scripts/controllers/NotesController.js
+++ Instant_AngularJS_Starter/guidebook/app/scripts/controllers/NotesController.js
@@ -1,3 +1,28 @@
+'use strict';
+
 /**
- * Created by luhtonen on 07/10/14.
+ * @ngdoc function
+ * @name GuidebookApp.Guidebook.controller:AddNoteController
+ * @description
+ * # AddNoteController and DeleteNoteController
+ * Controllers of the GuidebookApp.Guidebook
  */
+angular.module('GuidebookApp.Guidebook')
+  .controller('AddNoteController', ['$scope', '$location', '$routeParams', 'NoteModel',
+    function ($scope, $location, $routeParams, NoteModel) {
+      var chapterId = $routeParams.chapterId;
+      $scope.cancel = function() {
+        $location.path('/guidebook/chapter/' + chapterId);
+      };
+      $scope.createNote = function() {
+        NoteModel.addNote(chapterId, $scope.note.content);
+        $location.path('/guidebook/chapter/' + chapterId);
+      };
+  }])
+  .controller('DeleteNoteController', ['$scope', '$location', '$routeParams', 'NoteModel',
+    function($scope, $location, $routeParams, NoteModel) {
+      var chapterId = $routeParams.chapterId;
+      NoteModel.deleteNote(chapterId, $routeParams.noteId);
+      $location.path('/guidebook/chapter/' + chapterId);
+    }
+  ]);
