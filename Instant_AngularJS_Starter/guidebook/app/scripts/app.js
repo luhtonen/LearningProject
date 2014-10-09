d4c34fc90860d074fa8a64914b54c170577fd785
luhtonen <luhtonen@gmail.com>
2014-10-09 11:26:11 +0200

    applying active style to selected navigation tab

--- Instant_AngularJS_Starter/guidebook/app/scripts/app.js
+++ Instant_AngularJS_Starter/guidebook/app/scripts/app.js
@@ -27,4 +27,9 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  .controller('HeadersCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  }]);
--- Instant_AngularJS_Starter/guidebook/app/scripts/controllers/about.js
+++ Instant_AngularJS_Starter/guidebook/app/scripts/controllers/about.js
@@ -8,10 +8,5 @@
  * Controller of the GuidebookApp
  */
 angular.module('GuidebookApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AboutCtrl', ['$scope', function ($scope) {
+  }]);
