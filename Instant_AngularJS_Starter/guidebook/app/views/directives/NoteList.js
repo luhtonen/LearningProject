3395711ad4f3f474c0921d90a1f2b8f79c18af4a
luhtonen <luhtonen@gmail.com>
2014-10-10 12:28:43 +0200

    defining own scope for directive

--- Instant_AngularJS_Starter/guidebook/app/views/directives/NoteList.js
+++ Instant_AngularJS_Starter/guidebook/app/views/directives/NoteList.js
@@ -3,6 +3,12 @@
 angular.module('GuidebookApp.Guidebook').directive('gbNoteList', function() {
   return {
     restrict: 'E',
-    templateUrl: '/views/directives/noteList.html'
+    templateUrl: '/views/directives/noteList.html',
+    scope: {
+      show: '=show',
+      notes: '=notes',
+      orderValue: '@orderBy',
+      onDelete: '=deleteNoteHandler'
+    }
   };
 });
