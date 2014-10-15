effb851467dfc6edc86d0b918cf1b790d7583de9
luhtonen <luhtonen@gmail.com>
2014-10-15 15:22:27 +0200

    testing controller

--- MasteringWebAppDevWithAngularJS/ch02/js/admin-projects.js
+++ MasteringWebAppDevWithAngularJS/ch02/js/admin-projects.js
@@ -1,2 +1,12 @@
 angular.module('admin-projects', [])
-  .controller('ProjectEditCtrl');
+  .controller('ProjectsEditCtrl', function($scope, project) {
+    $scope.project = project;
+    $scope.removeTeamMember = function (teamMember) {
+      var idx = $scope.project.teamMembers.indexOf(teamMember);
+      if (idx >= 0) {
+        $scope.project.teamMembers.splice(idx, 1);
+      }
+    };
+
+    // other methods of the controller
+  });
