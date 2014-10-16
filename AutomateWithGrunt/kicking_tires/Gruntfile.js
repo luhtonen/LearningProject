b6c53d1df48b4e66e069751d99939b768dffe762
luhtonen <luhtonen@gmail.com>
2014-10-16 15:07:42 +0200

    adding error handling

--- AutomateWithGrunt/kicking_tires/Gruntfile.js
+++ AutomateWithGrunt/kicking_tires/Gruntfile.js
@@ -6,6 +6,9 @@ module.exports = function(grunt){
     grunt.log.writeln('Hi there, ' + name);
   });
   grunt.registerTask('addNumbers', function(first, second) {
+    if (isNaN(Number(first))) {
+      grunt.warn('The first argument must be a number.');
+    }
     var answer = Number(first) + Number(second);
     grunt.log.writeln(first + ' + ' + second + ' is ' + answer);
   });
