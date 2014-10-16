e8b5f135baa766c2e36ba75732d328c39aaa71ba
luhtonen <luhtonen@gmail.com>
2014-10-16 14:41:47 +0200

    creating task with parameters

--- AutomateWithGrunt/kicking_tires/Gruntfile.js
+++ AutomateWithGrunt/kicking_tires/Gruntfile.js
@@ -2,4 +2,7 @@ module.exports = function(grunt){
   grunt.registerTask('default', function() {
     grunt.log.writeln('Hello from Grunt.');
   });
+  grunt.registerTask('greet', function(name) {
+    grunt.log.writeln('Hi there, ' + name);
+  });
 }
