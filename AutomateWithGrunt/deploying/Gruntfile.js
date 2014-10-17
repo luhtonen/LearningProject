b168d605c36cc707747cf487a3d4cfbbe5f26e22
luhtonen <luhtonen@gmail.com>
2014-10-17 08:41:09 +0200

    adding task to copy files to working folder

--- AutomateWithGrunt/deploying/Gruntfile.js
+++ AutomateWithGrunt/deploying/Gruntfile.js
@@ -2,7 +2,10 @@ module.exports = function(grunt){
   grunt.config.init({
     copyFiles: {
       options: {
-        workingDirectory: 'working'
+        workingDirectory: 'working',
+        manifest: [
+          'index.html', 'stylesheets/style.css', 'javascripts/app.js'
+        ]
       }
     }
   });
@@ -13,5 +16,19 @@ module.exports = function(grunt){
   grunt.registerTask('clean', 'Deletes the working and its contents', function() {
     grunt.config.requires('copyFiles.options.workingDirectory');
     grunt.file.delete(grunt.config.get('copyFiles.options.workingDirectory'));
+  });
+  grunt.registerTask('copyFiles', 'Copy files to working folder', function() {
+    var files, workingDirectory;
+
+    grunt.config.requires('copyFiles.options.manifest');
+    grunt.config.requires('copyFiles.options.workingDirectory');
+
+    files = grunt.config.get('copyFiles.options.manifest');
+    workingDirectory = grunt.config.get('copyFiles.options.workingDirectory');
+    files.forEach(function(file) {
+      var destination = workingDirectory + '/' + file;
+      grunt.log.writeln('Copying ' + file + ' to ' + destination);
+      grunt.file.copy(file, destination);
+    });
   });
 };
