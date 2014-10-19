127be85866762d653d5acf62b4fcccf83dcca242
luhtonen <luhtonen@gmail.com>
2014-10-19 12:57:50 +0200

    creating plug-in to open file in Chrome

---
+++ AutomateWithGrunt/grunt-open-with-chrome/tasks/lib/chrome_launcher.js
@@ -0,0 +1,41 @@
+module.exports.init = function(grunt) {
+  // the object we'll return
+  var exports = {};
+
+  // creates the command
+  var createCommand = function(file) {
+    // booleans for the OS we're using
+    var command = '';
+    var linux = !!process.platform.match(/^linux/);
+    var windows = !!process.platform.match(/^win/);
+    if (windows) {
+      command = 'start chrome ' + file;
+    } else if (linux) {
+      command = 'google-chrome "' + file + '"';
+    } else {
+      command = 'open -a "Google Chrome" ' + file;
+    }
+    return command;
+  };
+
+  // opens Chrome and loads the file or URL passed in
+  exports.open = function(file, done) {
+    var command, process, exec;
+    command = createCommand(file);
+    grunt.log.writeln('Running command: ' + command);
+
+    exec = require('child_process').exec;
+    process = exec(command, function(error, stdout, stderr) {
+      if (error) {
+        if (error.code != 0) {
+          grunt.warn(stderr);
+          grunt.log.writeln(error.stack);
+        }
+      }
+      done();
+    });
+  };
+
+  // returns the object
+  return exports;
+};
---
+++ AutomateWithGrunt/grunt-open-with-chrome/tasks/open_with_chrome.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function (grunt) {
+  var chromeLauncher = require('./lib/chrome_launcher.js').init(grunt);
+  grunt.registerTask('open', 'Opens the file or URL with Chrome', function(file) {
+    var done = this.async();
+    chromeLauncher.open(file, done);
+  });
+};
