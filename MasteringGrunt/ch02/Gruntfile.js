4c51ce488f9223aad3322928f9246a221db241c4
luhtonen <luhtonen@gmail.com>
2014-10-21 14:51:57 +0200

    defining simple grunt task

--- MasteringGrunt/ch02/Gruntfile.js
+++ MasteringGrunt/ch02/Gruntfile.js
@@ -1,4 +1,19 @@
 'use strict';
 
 module.exports = function (grunt) {
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Project configuration
+  grunt.initConfig({
+    uglify: {
+      target1: {
+        src: 'src/foo.js',
+        dest: 'build/foo.min.js'
+      }
+    }
+  });
+
+  // Define the default task
+  grunt.registerTask('default', 'Default task', ['uglify']);
 };
---
+++ MasteringGrunt/ch02/src/foo.js
@@ -0,0 +1,4 @@
+var staff = function(some, awsome, cool, code) {
+  var even = 42, cooler = 7;
+  return some + even + cooler + code;
+};
