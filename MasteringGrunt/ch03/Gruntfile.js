d276f9aee8151d2b87090a687b1a2fd8acbe45cf
luhtonen <luhtonen@gmail.com>
2014-10-22 14:49:36 +0200

    experiments with multitask

--- MasteringGrunt/ch03/Gruntfile.js
+++ MasteringGrunt/ch03/Gruntfile.js
@@ -1,6 +1,22 @@
 'use strict';
 var fs = require('fs');
 module.exports = function (grunt) {
+  grunt.initConfig({
+    copy: {
+      target1: {
+        files: {
+          'dest/file1.txt': 'src/file1.txt',
+          'dest/file2.txt': 'src/file2.txt'
+        }
+      },
+      target2: {
+        files: {
+          'dest/file3.txt': 'src/file3.txt',
+          'dest/file4.txt': 'src/file4.txt'
+        }
+      }
+    }
+  });
   grunt.registerTask('log-deploy', 'Log deployment information to keep history', function() {
     var message = 'Deployment on ' + new Date();
     fs.appendFileSync('deploy.log', message + '\n');
@@ -19,4 +35,10 @@ module.exports = function (grunt) {
     console.log('uploading...');
   });
   grunt.registerTask('deploy', 'Deploy project', ['build', 'test', 'upload']);
+  grunt.registerMultiTask('copy', 'Copy files', function() {
+    this.files.forEach(function(file) {
+      grunt.file.copy(file.src, file.dest);
+    });
+    grunt.log.writeln('Copied ' + this.files.length + ' files');
+  });
 };
