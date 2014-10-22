165c6057b33068287d37fc7e001dc630e25e8927
luhtonen <luhtonen@gmail.com>
2014-10-22 15:58:58 +0200

    experiments with asynchronous task

--- MasteringGrunt/ch03/Gruntfile.js
+++ MasteringGrunt/ch03/Gruntfile.js
@@ -1,5 +1,7 @@
 'use strict';
 var fs = require('fs');
+var request = require('request');
+var url = 'https://raw.github.com/jpillora/gswg-examples/master/README.md';
 module.exports = function (grunt) {
   grunt.initConfig({
     copy: {
@@ -40,5 +42,19 @@ module.exports = function (grunt) {
       grunt.file.copy(file.src, file.dest);
     });
     grunt.log.writeln('Copied ' + this.files.length + ' files');
+  });
+  grunt.registerTask('webget', 'Getting file using HTTP', function() {
+    var done = this.async();
+    request(url, function(err, response, contents) {
+      if (err) {
+        done(err);
+      } else if (response.statusCode !== 200) {
+        done(new Error('Not OK'));
+      } else {
+        grunt.file.write('FILE.md', contents);
+        grunt.log.ok('FILE.md successfully created');
+        done();
+      }
+    });
   });
 };
