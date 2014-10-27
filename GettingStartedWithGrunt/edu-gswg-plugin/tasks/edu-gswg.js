35681af57b4901e62950d7a0854190f324cd7a11
luhtonen <luhtonen@gmail.com>
2014-10-27 12:14:48 +0100

    sample plugin initial version

---
+++ GettingStartedWithGrunt/edu-gswg-plugin/tasks/edu-gswg.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.registerTask('edu-gswg', '', function() {
+    grunt.log.ok("Hello, you have successfully run the 'edu-gswg' task.");
+  });
+};
