1bd2927487a60c7e025d21f576ab11e8e254d571
luhtonen <luhtonen@gmail.com>
2014-10-29 10:13:24 +0100

    fixing build

--- MasteringGrunt/Chapter1/Gruntfile.js
+++ MasteringGrunt/Chapter1/Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
     },
     usemin: {
       options: {
-        dirs: ['dist']
+        assetsDirs: ['dist']
       },
       html: ['dist/{,*/}*.html']
     },
