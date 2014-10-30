8c7fb3017b9f7ed2df511a94b8b06f9c45d95831
luhtonen <luhtonen@gmail.com>
2014-10-30 16:52:39 +0100

    configuring compass and coffee tasks

--- MasteringGrunt/Chapter3/Gruntfile.js
+++ MasteringGrunt/Chapter3/Gruntfile.js
@@ -24,6 +24,36 @@ module.exports = function (grunt) {
         files: ['<%= chapter3.app %>/styles/{,*/}*.{scss,sass}'],
         tasks: ['compass:server']
       }
+    },
+    compass: {
+      options: {
+        sassDir: '<%= chapter3.app %>/styles/sass',
+        cssDir: '<%= chapter3.app %>/styles',
+        importPath: '<%= chapter3.app %>/bower_components',
+        relativeAssets: false
+      },
+      dist: {},
+      server: {}
+    },
+    coffee: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= chapter3.app %>/coffee',
+          src: '{,*/}*.coffee',
+          dest: '<%= chapter3.dist %>/scripts',
+          ext: '.js'
+        }]
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: '<%= chapter3.app %>/coffee',
+          src: '{,*/}*.coffee',
+          dest: '<%= chapter3.app %>/scripts',
+          ext: '.js'
+        }]
+      }
     }
   });
 
