251471fcecf9c946e5b9ae69c29182ee2286e786
luhtonen <luhtonen@gmail.com>
2014-10-31 15:49:09 +0100

    configuring watch task

--- MasteringGrunt/Chapter4/Gruntfile.js
+++ MasteringGrunt/Chapter4/Gruntfile.js
@@ -10,7 +10,76 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     // Project settings
-    chapter4: appConfig
+    chapter4: appConfig,
+
+    watch: {
+      options: {
+        nospawn: true
+      },
+      coffee: {
+        files: ['<%= chapter4.app %>/coffee/{,*/}*.coffee'],
+        tasks: ['coffee:server']
+      },
+      compass: {
+        files: ['<%= chapter4.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['compass:server']
+      },
+      jade: {
+        files: ['<%= chapter4.app %>/templates/{,*/}*.jade'],
+        tasks: ['jade:server']
+      }
+    },
+    compass: {
+      options: {
+        sassDir: '<%= chapter4.app %>/styles/sass',
+        cssDir: '<%= chapter4.app %>/css',
+        importPath: '<%= chapter4.app %>/bower_components',
+        relativeAssets: false
+      },
+      dist: {},
+      server: {}
+    },
+    jade: {
+      dist: {
+        options: {
+          pretty: true
+        },
+        files: {
+          '<%= chapter4.app %>/index.html': '<%= chapter4.app %>/templates/index.jade'
+        }
+      },
+      server: {
+        options: {
+          data: {
+            debug: false
+          },
+          pretty: true
+        },
+        files: {
+          '<%= chapter4.app %>/index.html': '<%= chapter4.app %>/templates/index.jade'
+        }
+      }
+    },
+    coffee: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= chapter4.app %>/coffee',
+          src: '{,*/}*.coffee',
+          dest: '<%= chapter4.dist %>/scripts',
+          ext: '.js'
+        }]
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: '<%= chapter4.app %>/coffee',
+          src: '{,*/}*.coffee',
+          dest: '<%= chapter4.app %>/scripts',
+          ext: '.js'
+        }]
+      }
+    }
   });
 
   grunt.registerTask('build', [
