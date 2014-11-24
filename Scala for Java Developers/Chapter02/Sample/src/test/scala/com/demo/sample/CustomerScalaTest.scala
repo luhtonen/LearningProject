216aed99d003fe3f08a72ecab882b7501bbffc09
luhtonen <luhtonen@gmail.com>
2014-11-24 10:14:27 +0100

    Creating Scala test class

---
+++ Scala for Java Developers/Chapter02/Sample/src/test/scala/com/demo/sample/CustomerScalaTest.scala
@@ -0,0 +1,21 @@
+package com.demo.sample
+
+import org.junit._
+import Assert._
+
+class CustomerScalaTest {
+    @Before
+    def setUp: Unit = {}
+
+    @After
+    def tearDown: Unit = {}
+
+    @Test
+    def testGetCustomerId = {
+        System.out.println("getCustomerId")
+        val instance = new Customer()
+        val expResult: Integer = null
+        var result: Integer = instance.getCustomerId()
+        assertEquals(expResult, result)
+    }
+}
