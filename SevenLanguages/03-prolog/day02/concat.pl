a333fa71c395acf8e1154466c503023c38fd3ed1
luhtonen <luhtonen@gmail.com>
2014-12-08 19:08:29 +0100

    concatenate lists in Prolog, final version

--- SevenLanguages/03-prolog/day02/concat.pl
+++ SevenLanguages/03-prolog/day02/concat.pl
@@ -1,4 +1,3 @@
 concatenate([], List, List).
-concatenate([Head|[]], List, [Head|List]).
-concatenate([Head1|[Head2|[]]], List, [Head1, Head2|List]).
-concatenate([Head1|[Head2|[Head3|[]]]], List, [Head1, Head2, Head3|List]).
+concatenate([Head|Tail1], List, [Head|Tail2]) :-
+    concatenate(Tail1, List, Tail2).
