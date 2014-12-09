e1b2e2eaf21615d0a2596a6447a4667f4070f31f
luhtonen <luhtonen@gmail.com>
2014-12-09 09:54:52 +0100

    sudoku 4, final version

--- SevenLanguages/03-prolog/day03/sudoku4.pl
+++ SevenLanguages/03-prolog/day03/sudoku4.pl
@@ -1,3 +1,8 @@
+valid([]).
+valid([Head|Tail]) :-
+    fd_all_different(Head),
+    valid(Tail).
+
 sudoku(Puzzle, Solution) :-
     Solution = Puzzle,
     Puzzle = [S11, S12, S13, S14,
@@ -19,4 +24,8 @@ sudoku(Puzzle, Solution) :-
     Square1 = [S11, S12, S21, S22],
     Square2 = [S13, S14, S23, S24],
     Square3 = [S31, S32, S41, S42],
-    Square4 = [S33, S34, S43, S44].
+    Square4 = [S33, S34, S43, S44],
+
+    valid([Row1, Row2, Row3, Row4,
+           Col1, Col2, Col3, Col4,
+           Square1, Square2, Square3, Square4]).
