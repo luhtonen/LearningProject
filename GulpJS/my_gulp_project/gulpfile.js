a226e04fb47712f853510d97cde4ee6beb8636b0
luhtonen <luhtonen@gmail.com>
2014-12-11 19:21:51 +0100

    defining first gulp task using sass plugin

--- GulpJS/my_gulp_project/gulpfile.js
+++ GulpJS/my_gulp_project/gulpfile.js
@@ -1,1 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass');
+
+// Define path to sass
+var sassRoot = 'build/sass/';
+
+// Gulp task
+gulp.task('sass-to-css', function() {
+  return gulp.src(sassRoot + 'main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'));
+});
