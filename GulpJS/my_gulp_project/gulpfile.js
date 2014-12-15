c491f76c6f434ccd95baa677365da34e84730830
luhtonen <luhtonen@gmail.com>
2014-12-15 09:00:22 +0100

    combining tasks with Gulp

--- GulpJS/my_gulp_project/gulpfile.js
+++ GulpJS/my_gulp_project/gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass');
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  prefix = require('gulp-autoprefixer');
 
 // Define path to sass
 var sassRoot = 'build/sass/';
@@ -7,6 +9,12 @@ var sassRoot = 'build/sass/';
 // Gulp task
 gulp.task('sass-to-css', function() {
   return gulp.src(sassRoot + 'main.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/css'))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(prefix('last 3 version'))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('public/css'));
 });
