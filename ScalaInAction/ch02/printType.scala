a93ebf2c37719cb6cc5a3290368be10671046412
luhtonen <luhtonen@gmail.com>
2015-01-07 10:10:39 +0100

    Exception handling in Scala

--- ScalaInAction/ch02/printType.scala
+++ ScalaInAction/ch02/printType.scala
@@ -10,3 +10,9 @@ def rangeMatcher(num: Int) = num match {
     case within100 if within100 <= 100 => println("with in 11 to 100")
     case beyond100 if beyond100 < Integer.MAX_VALUE => println("beyond 100")
 }
+
+def rangeMatcher1(num: Int) = num match {
+    case within10 if within10 <= 10 => println("with in 0 to 10")
+    case within100 if within100 <= 100 => println("with in 11 to 100")
+    case _ => throw new IllegalArgumentException("Only values between 0 and 100 are allowed")
+}
