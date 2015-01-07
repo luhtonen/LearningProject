62cec42ba51e9acc1d3f06638e5131e638a2cf00
luhtonen <luhtonen@gmail.com>
2015-01-07 08:44:34 +0100

    matching range in Scala

--- ScalaInAction/ch02/printType.scala
+++ ScalaInAction/ch02/printType.scala
@@ -4,3 +4,9 @@ def printType(obj: AnyRef) = obj match {
     case a: Array[_] => println("This is Array")
     case d: java.util.Date => println("This is date")
 }
+
+def rangeMatcher(num: Int) = num match {
+    case within10 if within10 <= 10 => println("with in 0 to 10")
+    case within100 if within100 <= 100 => println("with in 11 to 100")
+    case beyond100 if beyond100 < Integer.MAX_VALUE => println("beyond 100")
+}
