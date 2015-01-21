b90af45b5cd3faf9024d5e3b1fb6cfb8d99ca183
luhtonen <luhtonen@gmail.com>
2015-01-21 08:27:56 +0100

    Adding assembly plugin support

--- ScalaForJavaDevelopers/Chapter02/Sample/target/generated-sources/annotations/com/demo/sample/MicroMarket_.java
+++ ScalaForJavaDevelopers/Chapter02/Sample/target/generated-sources/annotations/com/demo/sample/MicroMarket_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-24T11:15:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-20T08:53:19")
 @StaticMetamodel(MicroMarket.class)
 public class MicroMarket_ { 
 
