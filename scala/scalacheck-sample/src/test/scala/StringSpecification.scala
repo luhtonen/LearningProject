97c004ca26fa6590dd3ad933acaa9549ea6dcf85
luhtonen <luhtonen@gmail.com>
2015-01-26 18:18:20 +0100

    Scalacheck sample

---
+++ scala/scalacheck-sample/src/test/scala/StringSpecification.scala
@@ -0,0 +1,20 @@
+import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+
+/**
+ * Created by luhtonen on 26/01/15.
+ */
+object StringSpecification extends Properties("String") {
+
+  property("startsWith") = forAll { (a: String, b: String) =>
+    (a+b).startsWith(a)
+  }
+
+  property("concatenate") = forAll { (a: String, b: String) =>
+    (a+b).length > a.length && (a+b).length > b.length
+  }
+
+  property("substring") = forAll { (a: String, b: String, c: String) =>
+    (a+b+c).substring(a.length, a.length+b.length) == b
+  }
+}
