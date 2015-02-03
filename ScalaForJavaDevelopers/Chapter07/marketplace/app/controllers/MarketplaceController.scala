3f65b20a841b92a6c5aecda7c71c73dd6e764f14
Eduard Luhtonen <luhtonen@gmail.com>
2015-02-03 16:15:19 +0100

    implementing web service call

--- ScalaForJavaDevelopers/Chapter07/marketplace/app/controllers/MarketplaceController.scala
+++ ScalaForJavaDevelopers/Chapter07/marketplace/app/controllers/MarketplaceController.scala
@@ -1,8 +1,12 @@
 package controllers
 
-import play.api.libs.json.JsError
+import play.api.Play.current
+import play.api.mvc._
 import play.api.libs.ws.WS
-import play.api.mvc.{Action, Controller}
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.json._
+import models._
+import views._
 
 /**
  * Created by luhtonen on 03/02/15.
@@ -12,24 +16,25 @@ object MarketplaceController extends Controller {
   val pageSize = 10
   val appStoreUrl = "https://itunes.apple.com/search"
 
-  def list(page: Int, orderBy: Ing, filter: String = "*") = Action.
-    async { implicit request =>
-      val futureWSResponse = WS.url(appStoreUrl)
-        .withQueryString("term" -> filter, "country" -> "fi", "entity" -> "software").get()
+  def list(page: Int, orderBy: Int, filter: String = "*") = Action.async { implicit request =>
+      println("list page: " + page + ", orderBy: " + orderBy + ", filter: " + filter)
+      val futureWSResponse =
+        WS.url(appStoreUrl)
+          .withQueryString("term" -> filter, "country" -> "fi", "entity" -> "software")
+          .get()
 
       futureWSResponse map { resp =>
         val json = resp.json
         val jsResult = json.validate[AppResult]
-        jsResult map {
+        jsResult.map {
           case AppResult(count, res) =>
             Ok(html.marketplace.list(
               Page(res,
-                   page,
-                   offset = pageSize * page,
-                   count),
+                page,
+                offset = pageSize * page,
+                count),
               orderBy,
-              filter
-            ))
+              filter))
         }.recoverTotal {
           e => BadRequest("Detected error: " + JsError.toFlatJson(e))
         }
