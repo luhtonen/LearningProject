36ad026b9b8c2cbad933d0120cd8ff2a9370de9a
luhtonen <luhtonen@gmail.com>
2015-03-03 08:05:57 +0100

    passing wrong parameter type
    TypeScript compiler notice type mismatch and produce error message

---
+++ TypeScriptTutorial/greeter2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by luhtonen on 03/03/15.
+ */
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = [1, 2, 3];
+document.body.innerHTML = greeter(user);
---
+++ TypeScriptTutorial/greeter2.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by luhtonen on 03/03/15.
+ */
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+var user = [1, 2, 3];
+
+document.body.innerHTML = greeter(user);
+
+// compile error:
+// greeter2.ts(10,35): error TS2345: Argument of type 'number[]' is not assignable to parameter of type 'string'.
