73211311f01149bc963f3367f7fc8aa4d864d65b
luhtonen <luhtonen@gmail.com>
2015-03-03 08:39:47 +0100

    Adding class for TypeScript

---
+++ TypeScriptTutorial/student.js
@@ -0,0 +1,17 @@
+/**
+ * Created by luhtonen on 03/03/15.
+ */
+var Student = (function () {
+    function Student(firstname, middleintial, lastname) {
+        this.firstname = firstname;
+        this.middleintial = middleintial;
+        this.lastname = lastname;
+        this.fullname = firstname + " " + middleintial + " " + lastname;
+    }
+    return Student;
+})();
+function greeter(person) {
+    return "Hello, " + person.firstname + " " + person.lastname;
+}
+var user = new Student("Edu", "E", "Finn");
+document.body.innerHTML = greeter(user);
---
+++ TypeScriptTutorial/student.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by luhtonen on 03/03/15.
+ */
+class Student {
+  fullname: string;
+  constructor(public firstname, public middleintial, public lastname) {
+    this.fullname = firstname + " " + middleintial + " " + lastname;
+  }
+}
+
+interface Person {
+  firstname: string;
+  lastname: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstname + " " + person.lastname;
+}
+
+var user = new Student("Edu", "E", "Finn");
+
+document.body.innerHTML = greeter(user);
