bd7d903d76f4379ea4515feb18d2973019f4d084
luhtonen <luhtonen@gmail.com>
2015-03-04 09:25:10 +0100

    first node.js script
    I’m started reading article Mastering MEAN on IBM’s site:
    http://www.ibm.com/developerworks/library/wa-mean1/index.html

---
+++ mastering-mean/test.js
@@ -0,0 +1,19 @@
+/**
+ * Created by luhtonen on 04/03/15.
+ */
+console.log(process.versions);
+console.log(process.arch);
+console.log(process.platform);
+
+/*
+Output:
+ { http_parser: '2.3',
+ node: '0.12.0',
+ v8: '3.28.73',
+ uv: '1.0.2',
+ zlib: '1.2.8',
+ modules: '14',
+ openssl: '1.0.1l' }
+ x64
+ darwin
+ */
